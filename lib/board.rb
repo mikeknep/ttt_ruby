3209f800d7d30c09c1cbe4bac769183f40ade55b
Mike Knepper <michaelrknepper@gmail.com>
2014-02-19 13:40:14 -0600

    Board can determine values for rows, columns, and diagonals

--- lib/board.rb
+++ lib/board.rb
@@ -5,4 +5,77 @@ class Board
     @size = size
     @spots = Array.new(size**2, ' ')
   end
+
+
+  def all_spots_taken?
+    true if not spots.include?(' ')
+  end
+
+
+  def rows
+    row_template = Array.new
+    size.times do |i|
+      row_template << i
+    end
+
+    all_rows = Array.new
+    size.times do |i|
+      all_rows << row_template.map{|t| t + size*i}
+    end
+
+    return all_rows
+  end
+
+ 
+  def columns
+    column_template = Array.new
+    size.times do |i|
+      column_template << size*i
+    end
+
+    all_columns = Array.new
+    size.times do |i|
+      all_columns << column_template.map{|t| t + i}
+    end
+
+    return all_columns
+  end
+
+
+  def diagonals
+    d1_template = Array.new
+    size.times do |i|
+      d1_template << (size-1)*(i+1)
+    end
+
+    d2_template = Array.new
+    size.times do |i|
+      d2_template << (size+1)*i
+    end
+
+    return [d1_template, d2_template]
+  end
+
+
+  def get_values_for(direction)
+    values = Array.new
+    direction.each do |dir|
+      values << dir.map{|i| spots.values_at(i) }.flatten
+    end
+    return values
+  end
+
+
+  def winner?(paths)
+    winner = false
+    
+    paths.each do |path|
+      if path.uniq.length == 1 && path[0] != ' '
+        winner = true
+      end
+    end
+
+    return winner
+  end
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,11 +1,38 @@
 require 'spec_helper'
 
 describe Board do
+  let(:board)   { Board.new(3) }
   it "has a size that defines the number of rows and columns" do
-    expect(Board.new(3).size).to eq(3)
+    expect(board.size).to eq(3)
   end
 
   it "has a collection of spots equal to the square of the size" do
-    expect(Board.new(3).spots.count).to eq(9)
+    expect(board.spots.count).to eq(9)
+  end
+
+  it "determines if all spots are taken" do
+    board.spots = Array.new(9, ['X','O'].sample)
+
+    expect(board.all_spots_taken?).to eq(true)
+  end
+
+  context "checking for a winner" do
+    it "determines if someone has won horizontally" do
+      board.spots = ['X','X','X',' ',' ',' ',' ',' ',' ']
+
+      expect(board.winner?(board.get_values_for(board.rows))).to eq(true)
+    end
+
+    it "determines if someone has won vertically" do
+      board.spots = ['X',' ',' ','X',' ',' ','X',' ',' ']
+
+      expect(board.winner?(board.get_values_for(board.columns))).to eq(true)
+    end
+
+    it "determines if someone has won diagonally" do
+      board.spots = ['X',' ',' ',' ','X',' ',' ',' ','X']
+
+      expect(board.winner?(board.get_values_for(board.diagonals))).to eq(true)
+    end
   end
 end
