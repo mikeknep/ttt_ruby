2fec3e7678a2ba985ff96704dfff57438ba111eb
Mike Knepper <michaelrknepper@gmail.com>
2014-02-22 13:39:25 -0600

    Removes game over methods from game class (in the Rules now)

--- lib/game.rb
+++ lib/game.rb
@@ -6,22 +6,4 @@ class Game
     @player_1 = params[:player_1]
     @player_2 = params[:player_2]
   end
-
-  def over?
-    board.all_spots_taken? || board.winner?
-  end
-
-  def current_player
-    count = 0
-
-    board.spots.each do |spot|
-      count += 1 if spot != " "
-    end
-
-    if count % 2 == 0
-      return player_1
-    else
-      return player_2
-    end
-  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -14,24 +14,4 @@ describe Game do
   it "has a player 2" do
     expect(game.player_2).to be_a(Player)
   end
-
-  describe "checks if it is over" do
-    it "is over when all spots have been played" do
-      game.board.spots = ['X','X','O','O','O','X','X','O','X']
-
-      expect(game.over?).to eq(true)
-    end
-
-    it "is over when someone has won" do
-      game.board.spots = ['X','X','X',' ',' ',' ',' ',' ',' ']
-
-      expect(game.over?).to eq(true)
-    end
-  end
-
-  it "identifies the current player" do
-    game.board.spots = ['X',' ',' ',' ',' ',' ',' ',' ',' ']
-
-    expect(game.current_player).to eq(game.player_2)
-  end
 end
