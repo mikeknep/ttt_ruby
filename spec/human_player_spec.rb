38684a45b4773c26a0113cf0d8207c47137c43bf
Mike Knepper <michaelrknepper@gmail.com>
2014-02-22 17:35:51 -0600

    Aesthetic improvement to spec

--- spec/human_player_spec.rb
+++ spec/human_player_spec.rb
@@ -3,15 +3,15 @@ require 'stringio'
 
 describe HumanPlayer do
   let(:human)   { HumanPlayer.new('X') }
-  let(:board)   { 
-    $stdin = StringIO.new('0')
-    Board.new(2) }
+  let(:board)   { Board.new(2) }
 
   it "has a token" do
     expect(human.token).to eq('X')
   end
 
   it "takes a turn" do
+    $stdin = StringIO.new('0')
+
     expect {
       human.take_turn(board)
     }.to change{board.spots}
