a7e33e1477b600a366f14b3908cb5c1916be7a4f
Mike Knepper <michaelrknepper@gmail.com>
2014-02-22 16:23:23 -0600

    Removes old Player classes

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 
 describe Game do
-  let(:game)  { Game.new(board_size: 3, player_1: Human.new('X'), player_2: Jane.new('O')) }
+  let(:game)  { Game.new(board_size: 3, player_1: HumanPlayer.new('X'), player_2: JanePlayer.new('O')) }
 
   it "has a board" do
     expect(game.board).to be_a(Board)
   end
 
   it "has a player 1" do
-    expect(game.player_1).to be_a(Player)
+    expect(game.player_1).to be_a(HumanPlayer)
   end
 
   it "has a player 2" do
-    expect(game.player_2).to be_a(Player)
+    expect(game.player_2).to be_a(JanePlayer)
   end
 end
