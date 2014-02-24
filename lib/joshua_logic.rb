8c05dc55415d6b7641a781320586cf19d3d7c752
Mike Knepper <michaelrknepper@gmail.com>
2014-02-24 14:34:12 -0600

    Deletes Joshua framework. Moving to a new branch

--- lib/joshua_logic.rb
+++
@@ -1,5 +0,0 @@
-module JoshuaLogic
-  def self.choose_next_move(board)
-    board.spots.index { |spot| spot == ' ' } # This line is the same as Jane's logic -- just take the first open spot on the board
-  end
-end
--- spec/joshua_logic_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe JoshuaLogic do
-  let(:board)   { Board.new(3) }
-
-  it "chooses the next move"
-
-end
