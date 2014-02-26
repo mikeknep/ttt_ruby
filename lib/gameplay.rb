fdd1571b793404a34917805bc072cb7dbf31c73d
Mike Knepper <michaelrknepper@gmail.com>
2014-02-26 14:47:51 -0600

    Gameplay only executes if move is valid

--- lib/gameplay.rb
+++ lib/gameplay.rb
@@ -1,5 +1,9 @@
 module Gameplay
   def self.take_turn(board, spot, token)
-    board.spots[spot] = token
+    board.spots[spot] = token if valid_spot?(board, spot)
+  end
+
+  def self.valid_spot?(board, spot)
+    board.spots[spot] == ' '
   end
 end
--- spec/gameplay_spec.rb
+++ spec/gameplay_spec.rb
@@ -8,4 +8,23 @@ describe Gameplay do
       Gameplay.take_turn(board, 4, 'X')
     }.to change{board.spots}
   end
+
+  it "returns false for an invalid move" do
+    board.spots[3] = 'X'
+
+    expect(Gameplay.valid_spot?(board, 3)).to eq(false)
+  end
+
+  it "returns true for a valid move" do
+    board.spots[3] = 'X'
+
+    expect(Gameplay.valid_spot?(board, 5)).to eq(true)
+  end
+
+  it "doesn't play a turn if the spot is invalid" do
+    board.spots[3] = 'X'
+    expect {
+      Gameplay.take_turn(board, 3, 'O')
+    }.to_not change{board.spots}
+  end
 end
