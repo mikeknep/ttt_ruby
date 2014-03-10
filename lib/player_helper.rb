5b6a83a75c987f0b3f20fd0a6ffdd6a019b49f68
Mike Knepper <michaelrknepper@gmail.com>
2014-03-10 10:08:29 -0500

    Player helper builds instances of new player class

--- lib/player_helper.rb
+++ lib/player_helper.rb
@@ -1,6 +1,14 @@
 module PlayerHelper
-  def self.build_player(name, token)
-    class_name = name.capitalize + "Player"
-    Object.const_get(class_name).new(token)
+  def self.build_player(token, intelligence)
+    decision_maker = case intelligence
+    when 'easy'
+      SimpleAI
+    when 'hard'
+      UnbeatableAI
+    when 'human'
+      ConsoleUI
+    end
+
+    Player.new(token, decision_maker)
   end
 end
--- spec/player_helper_spec.rb
+++ spec/player_helper_spec.rb
@@ -1,7 +1,15 @@
 require 'spec_helper'
 
 describe PlayerHelper do
-  it "builds a specific kind of player with a custom token" do
-    expect(PlayerHelper.build_player('Jane', 'M')).to be_a(JanePlayer)
+  it "builds a human player with a token" do
+    expect(PlayerHelper.build_player('X', 'human')).to be_a(Player)
+  end
+
+  it "builds an easy computer player with a token" do
+    expect(PlayerHelper.build_player('O', 'easy')).to be_a(Player)
+  end
+
+  it "builds an unbeatable computer player with a token" do
+    expect(PlayerHelper.build_player('O', 'hard')).to be_a(Player)
   end
 end
